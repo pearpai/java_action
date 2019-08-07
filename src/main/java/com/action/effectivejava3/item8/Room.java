8d1ca01f6d714e3a095a08513806c62d6cdeaa51
pearpai <469901870@qq.com>
2019-08-07 14:36:45 +0800

    nio  学习

---
+++ src/main/java/com/action/effectivejava3/item8/Room.java
@@ -0,0 +1,16 @@
+package com.action.effectivejava3.item8;
+//import java.lang.ref.Cleaner;
+/**
+ * Summary:
+ * Created by wuyunfeng on 07. Aug 2019 9:08 AM.
+ */
+public class Room implements AutoCloseable {
+
+//    private static final Cleaner cleaner = Cleaner.create();
+
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
