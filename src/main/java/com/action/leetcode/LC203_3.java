e2afe2a28e40439d18e7f0dbca9e398289986b5c
pearpai <469901870@qq.com>
2019-11-11 15:08:49 +0800

    no message

--- src/main/java/com/action/leetcode/LC203_3.java
+++ src/main/java/com/action/leetcode/LC203_3.java
@@ -25,4 +25,20 @@ public class LC203_3 {
         }
     }
 
+    public static void main(String[] args) {
+        System.out.println(sum(new int[]{1, 2, 3, 4, 5, 6, 7, 8}));
+    }
+
+    private static int sum(int[] arr) {
+        return sum(arr, 0);
+    }
+
+    private static int sum(int[] arr, int l) {
+
+        if (arr.length == l) {
+            return 0;
+        }
+        return arr[l] + sum(arr, l + 1);
+    }
+
 }
